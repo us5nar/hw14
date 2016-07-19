e5e4687bf7493b1ca393f65e0fe4c17d98946dbc
US5NAR <us5nar@gmail.com>
2016-07-20 00:30:16 +0300

    clean progect structure

---
+++ HomeWork14/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeWork14
+{
+    //represents some perosns' arguments
+    public class Person
+    {
+        public string Name;
+        public int Age;
+    }
+    public class PersonProperties:EventArgs
+    {
+        public Person person;
+    }
+}
---
+++ HomeWork14/PersonSaver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14
+{
+    //see filter from Lesson12
+    class PersonSaver
+    {
+    }
+}
